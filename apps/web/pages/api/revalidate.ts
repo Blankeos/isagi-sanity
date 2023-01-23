f8fee887daa16f9252f2de5ae0c847fe6e61a365
Blankeos <carloantonioct@gmail.com>
2023-01-24 02:13:41 +0800

    fix: sanity webhook integration using @sanity/webhook

--- apps/web/pages/api/revalidate.ts
+++ apps/web/pages/api/revalidate.ts
@@ -1,28 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+import { Readable } from "stream";
+
+const secret = process.env.MY_SECRET_TOKEN || "INVALID_TOKEN";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log({
-    request: req,
-    secretReceived: req.query.secret,
-    slugReceived: req.query.slug,
-    requestBody: req.body,
-  });
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
+
+  // (-Original Docs-)
+  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
+
+  // (-@sanity/webhook Docs- https://github.com/sanity-io/webhook-toolkit)
+  const signature: any = req.headers[SIGNATURE_HEADER_NAME];
+  const body = await readBody(req); // Read the body into a string
+
+  if (!isValidSignature(body, signature, secret)) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid signature" });
   }
 
   try {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-    await res.revalidate(`/post/${req.query.slug}`);
+    await res.revalidate(`/post/${req.body.slug.current}`);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return res.status(500).send("Error revalidating");
   }
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+async function readBody(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
 }
